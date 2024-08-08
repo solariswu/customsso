1a2cbaad224596c2f0b580b7173d6e8e4dcc404f
yungangwu <solariswu@gmail.com>
2024-08-08 13:53:22 +1000

    move recaptcha key to front end config file

--- src/const.js
+++ src/const.js
@@ -4,6 +4,8 @@ const rootDomain = 'amfa.aws-amplify.dev';
 export const applicationUrl = 'https://apersona.netlify.app/';
 export const apiUrl = `https://api.${clientName}.${rootDomain}`;
 
+export const recaptcha_key = "6LdFWYUnAAAAAATdjfhjNZqDqPOEn6GWLKHWU9na";
+
 // UI STRINGS
 export const mfaPageTitle = 'Your login requires an additional verification';
 export const selfServicePageTitle = 'Update Profile';
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import FeConfigProvider from './DataProviders/FeConfigProvider';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import {GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
 import './bootstrap.css';
 import './index.css';
 import App from './App';
 import './i18n.js';
-
+import {recaptcha_key} from './const.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <FeConfigProvider>
-        < GoogleReCaptchaProvider reCaptchaKey="6LdFWYUnAAAAAATdjfhjNZqDqPOEn6GWLKHWU9na">
+        <GoogleReCaptchaProvider reCaptchaKey={recaptcha_key}>
           <App />
         </GoogleReCaptchaProvider>
       </FeConfigProvider>
