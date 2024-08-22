c026edb28fbbcac62d6265e82766b99c60acdc64
yungangwu <solariswu@gmail.com>
2024-08-22 15:06:04 +1000

    add secrets into installer config

--- config.sh
+++ config.sh
@@ -7,6 +7,19 @@ export TENANT_ID='' ## example 'amfa-dev006'
 export ROOT_DOMAIN_NAME='' ## example 'apersona2.aws-amplify.dev'
 export ROOT_HOSTED_ZONE_ID='' ## example 'Z0596003151J12345678X'
 
+# Registered values from APERSONA
+export MOBILE_TOKEN_KEY=''
+export MOBILE_TOKEN_SALT=''
+export ASM_SALT=''
+export TENANT_AUTH_TOKEN=''
+
+# Your SMTP server info for sending email to end users
+export SMTP_HOST='' ## example 'email-smtp.us-east-1.amazonaws.com'
+export SMTP_USER='' ## example '<YOUR_SMTP_SENDER_USERNANE>'
+export SMTP_PASS='' ## example 'BQj1234567890'
+export SMTP_SECRURE='false' ## example 'false'
+export SMTP_PORT='587' ## example '587'
+
 # optional
 export SP_PORTAL_URL='' ## example 'https://apersona.netlify.app' ## can be removed if not use service providers portal.
 export EXTRA_APP_URL='' ## example 'https://amfa.netlify.app/' ## can be removed if no extra application using AMFA controller.
