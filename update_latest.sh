e43082ea5a029aa379b623fe13acb85cac7d7280
Yungang <solariswu@gmail.com>
2025-04-09 22:12:48 +1000

    update sh updated

--- update_latest.sh
+++ update_latest.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 
 if ! [ -f ./config.sh ]; then
   echo "config.sh file does not exist."
@@ -6,9 +6,7 @@ if ! [ -f ./config.sh ]; then
 else
   APERSONAIDP_REPO_NAME=aPersona-Identity-for-AWS-End-User-Services
   APERSONAADM_REPO_NAME=aPersona-Identity-for-AWS-Admin-Portal
-  INIT_DEPLOY_SCRIPT=https://raw.githubusercontent.com/solariswu/free-cdn-source/main/init_deploy.sh
+  INIT_DEPLOY_SCRIPT=https://raw.githubusercontent.com/kcsapersona/aPersona-AWS-IDP-Installer/main/init_deploy.sh
 
-  mv config.sh .amfa_config_bk && rm -rf $APERSONAIDP_REPO_NAME/ $APERSONAADM_REPO_NAME/ *.sh *.json *.txt && curl -o- $INIT_DEPLOY_SCRIPT |bash && cp .amfa_config_bk config.sh && ./$APERSONAIDP_REPO_NAME/install.sh
+  mv -- config.sh .amfa_config_bk && rm -rf -- "$APERSONAIDP_REPO_NAME/" "$APERSONAADM_REPO_NAME/" ./*.sh ./*.json ./*.txt && curl -o- "$INIT_DEPLOY_SCRIPT" | bash && cp -- .amfa_config_bk config.sh && ./"$APERSONAIDP_REPO_NAME/install.sh"
 fi
-
-
