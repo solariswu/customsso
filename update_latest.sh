28eb1b4dbd9a0cefd314a03ca37183e6273a9efd
Yungang <solariswu@gmail.com>
2025-04-10 09:52:25 +1000

    correct install folder

--- update_latest.sh
+++ update_latest.sh
@@ -6,7 +6,7 @@ if ! [ -f ./config.sh ]; then
 else
   APERSONAIDP_REPO_NAME=aPersona-Identity-for-AWS-End-User-Services
   APERSONAADM_REPO_NAME=aPersona-Identity-for-AWS-Admin-Portal
-  INIT_DEPLOY_SCRIPT=https://raw.githubusercontent.com/kcsapersona/aPersona-AWS-IDP-Installer/main/init_deploy.sh
+  INIT_DEPLOY_SCRIPT=https://raw.githubusercontent.com/apersona/aPersona-Identity-for-AWS-Installation/main/init_deploy.sh
 
   mv -- config.sh .amfa_config_bk && rm -rf -- "$APERSONAIDP_REPO_NAME/" "$APERSONAADM_REPO_NAME/" ./*.sh ./*.json ./*.txt && curl -o- "$INIT_DEPLOY_SCRIPT" | bash && cp -- .amfa_config_bk config.sh && ./"$APERSONAIDP_REPO_NAME/install.sh"
 fi
